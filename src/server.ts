182bc92a0b9d0deaa784be3cb43e44812af2f911
Kayk magalhaes <kaykmagalhaesdiv@gmail.com>
2023-04-10 19:42:50 -0300

    Configuracao de server iniciada

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,15 @@
+import express from 'express'
+import {Router, Request, Response } from 'express'
 
+const app = express()
+const router = Router()
+app.use(express.json())
+
+
+router.get('/', (req:Request, res:Response) => {
+    res.json('Server is running!')
+})
+
+
+app.use(router)
+app.listen(3000, () => 'server is running in port 3000')
