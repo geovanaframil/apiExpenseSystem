dbca41e2318e112fa9ec40dc839c9bc8656a6877
Rheingantz <pedrorpiragibe@gmail.com>
2023-04-10 21:06:22 -0300

    import do getexpenser.ts para o server.ts

---
+++ src/get/getExpenses.ts
@@ -0,0 +1,11 @@
+import { Router, Request, Response } from "express";
+
+const router = Router()
+
+
+
+router.get('/expenses', (req:Request, res:Response) => {
+    res.json('Server is running!')
+})
+
+export default router
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import get from '../src/get/get'
+import getExpenses from '../src/get/getExpenses'
 
 const app = express()
 app.use(express.json())
 
 
 app.use(get)
+app.use(getExpenses)
 app.listen(3000, () => 'server is running in port 3000')
