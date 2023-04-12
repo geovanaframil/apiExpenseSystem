f0fb124981da16b58db847fdf399bbc75919fef1
Rheingantz <pedrorpiragibe@gmail.com>
2023-04-12 11:14:19 -0300

    implementação do postExpenses no server, utilizando as funções verifyBody, createIdByExpense, handleBodyExpense e saveData

--- src/post/postExpenses.ts
+++ src/post/postExpenses.ts
@@ -1,6 +1,8 @@
 import { Router, Request, Response } from "express";
 import createIdByExpense from "../utils/createIdByExpense";
 import IExpense from "../interfaces/expenseInterface";
+import verifyBody from "../utils/verifyBodyExpense";
+import saveData from "../utils/saveDataJson";
 
 const router = Router();
 
@@ -20,4 +22,15 @@ function handleBodyExpenseRegister(
 
 router.post("/expenses", (req: Request, res: Response) => {
   const body = req.body;
+  const bodyIsValid = verifyBody(body);
+  if (bodyIsValid.errors) {
+    res.json(bodyIsValid.errors);
+  } else {
+    body.id = createIdByExpense(body.name, body.userID);
+    const currentExpense = handleBodyExpenseRegister(body, body.id);
+    saveData(currentExpense, "expenses");
+    res.json({ message: `Despesa cadastrada com sucesso! ${body}` });
+  }
 });
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import getExpense from "./get/getExpenses";
+import postExpenses from "./post/postExpenses";
 
 const app = express();
 app.use(express.json());
 
 app.use(getExpense);
+app.use(postExpenses);
 
 app.listen(3000, () => "server is running in port 3000");
