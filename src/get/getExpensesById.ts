88a9b48e4aa931f882b92d1e9b7cbc1c71191e29
Rheingantz <pedrorpiragibe@gmail.com>
2023-04-12 20:40:25 -0300

    implementação do GET por ID

---
+++ src/get/getExpensesById.ts
@@ -0,0 +1,19 @@
+import { Router, Request, Response } from "express";
+import IExpense from "../interfaces/expenseInterface";
+import readData from "../utils/readDataJson";
+
+const router = Router();
+const data = readData("expenses");
+const expenses: IExpense[] = data;
+
+router.get("/expenses/:expenseID", (req: Request, res: Response) => {
+  const id = req.params.expenseID;
+  const expenseId = expenses.find((item) => item.id === id);
+  if (expenseId) {
+    return res.status(200).json(expenseId);
+  } else {
+    res.status(404).json(`id ${id} não existe`);
+  }
+});
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import getExpense from "./get/getExpenses";
+import getExpenseById from "./get/getExpensesById";
 import postExpenses from "./post/postExpenses";
+
 
 const app = express();
 app.use(express.json());
 
 app.use(getExpense);
+app.use(getExpenseById);
 app.use(postExpenses);
 
 app.listen(3000, () => "server is running in port 3000");
