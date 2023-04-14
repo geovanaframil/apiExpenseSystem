e919ad46bf7ad501387efe21ebf12dea276ef8a4
Geovana Framil <geovanaframil@hotmail.com>
2023-04-14 20:01:51 -0300

    Método put atualizando array

--- src/put/putExpenses.ts
+++ src/put/putExpenses.ts
@@ -7,39 +7,60 @@ const router = Router();
 const data = require("../../database/expenses.json");
 const expenses: IExpense[] = data;
 
-router.put("/expenses/:expensesID", (req: Request, res: Response) => {
-  const id = req.params.expensesID;
-  const body = req.body;
-  const expenseId = expenses.findIndex((item) => item.id === id);
-  if (expenseId === -1) {
-    return res
-      .status(404)
-      .json(`despesa correspondente ao id ${id} não existe`);
-  }
-
+function verifyAndUpdateExpenseBody(body: any) {
   const validProperties = ["name", "amount", "status"];
   const invalidProperties = Object.keys(body).filter(
     (prop) => !validProperties.includes(prop)
   );
 
   if (invalidProperties.length > 0) {
-    return res
-      .status(400)
-      .json(`Propriedades inválidas: ${invalidProperties.join(", ")}`);
+    return {
+      errors: `Propriedades inválidas: ${invalidProperties.join(", ")}`,
+    };
   }
 
-  const currentExpense = expenses[expenseId];
-  const updatedExpenses = expenses.map((expense) => {
-    if (expense.id === id) {
-      return {
-        ...currentExpense,
-        ...body,
-      };
-    }
-    return expense;
-  });
-  saveData(updatedExpenses, "expenses");
-  res.status(200).json(updatedExpenses);
+  // Se for fornecido, verifica se o amount é um número
+  if (body.name && typeof body.name !== "string") {
+    return { errors: "O conteúdo do nome deve ser uma string." };
+  }
+  if (body.amount && typeof body.amount !== "number") {
+    return { errors: "O valor da despesa deve ser um número." };
+  }
+  if (body.status && typeof body.status !== "string") {
+    return { errors: "O conteúdo do status deve ser uma string." };
+  }
+
+  // Retorna um objeto com as propriedades permitidas
+  return {
+    name: body.name,
+    amount: body.amount,
+    status: body.status,
+  };
+}
+
+router.put("/expenses/:expensesID", (req: Request, res: Response) => {
+  const id = req.params.expensesID;
+  const body = req.body;
+
+  const expenseIndex = expenses.findIndex((expense) => expense.id === id);
+  if (expenseIndex === -1) {
+    return res
+      .status(404)
+      .json(`despesa correspondente ao id ${id} não existe`);
+  }
+
+  const validatedBody = verifyAndUpdateExpenseBody(body);
+  if (validatedBody.errors) {
+    return res.status(400).json({ message: validatedBody.errors });
+  }
+
+  const currentExpense = expenses[expenseIndex];
+  const updatedExpense = { ...currentExpense, ...validatedBody };
+  expenses[expenseIndex] = updatedExpense;
+  res
+    .status(200)
+    .json({ message: "Despesa atualizada com sucesso", updatedExpense });
+  saveData(expenses, "expenses");
 });
 
 export default router;
