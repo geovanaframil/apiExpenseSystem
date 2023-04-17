22bb8f63cbe6f38f70069f74ec69b65d4f042be5
Rheingantz <pedrorpiragibe@gmail.com>
2023-04-17 19:27:58 -0300

    corrigindo rota PUT (tratando erro de referencia de memoria)

--- src/put/putExpenses.ts
+++ src/put/putExpenses.ts
@@ -19,6 +19,14 @@ function verifyAndUpdateExpenseBody(body: any) {
     };
   }
 
+  // Verifica cada propriedade e atualiza apenas se ela existir no objeto atualizado
+  const updatedBody : any= {};
+  for (const prop of validProperties) {
+    if (body[prop] !== undefined) {
+      updatedBody[prop] = body[prop];
+    }
+  }
+
   // Se for fornecido, verifica se o amount é um número
   if (body.name && typeof body.name !== "string") {
     return { errors: "O conteúdo do nome deve ser uma string." };
@@ -29,13 +37,8 @@ function verifyAndUpdateExpenseBody(body: any) {
   if (body.status && typeof body.status !== "string") {
     return { errors: "O conteúdo do status deve ser uma string." };
   }
-
-  // Retorna um objeto com as propriedades permitidas
-  return {
-    name: body.name,
-    amount: body.amount,
-    status: body.status,
-  };
+  // Retorna um objeto com as propriedades permitidas atualizadas
+  return updatedBody;
 }
 
 router.put("/expenses/:expensesID", (req: Request, res: Response) => {
@@ -49,14 +52,15 @@ router.put("/expenses/:expensesID", (req: Request, res: Response) => {
       .json(`despesa correspondente ao id ${id} não existe`);
   }
 
-  const validatedBody = verifyAndUpdateExpenseBody(body);
-  if (validatedBody.errors) {
-    return res.status(400).json({ message: validatedBody.errors });
+  const updatedBody = verifyAndUpdateExpenseBody(body);
+  if (updatedBody.errors) {
+    return res.status(400).json({ message: updatedBody.errors });
   }
 
   const currentExpense = expenses[expenseIndex];
-  const updatedExpense = { ...currentExpense, ...validatedBody };
+  const updatedExpense = { ...currentExpense, ...updatedBody };
   expenses[expenseIndex] = updatedExpense;
+
   res
     .status(200)
     .json({ message: "Despesa atualizada com sucesso", updatedExpense });
