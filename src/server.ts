d82e012de8300c464f2b0a7f8ab20215a4995172
Rheingantz <pedrorpiragibe@gmail.com>
2023-04-17 19:47:56 -0300

    implementando a rota delete

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import getExpense from "./get/getExpenses";
 import getExpenseById from "./get/getExpensesById";
 import postExpenses from "./post/postExpenses";
 import putExpenses from "./put/putExpenses";
+import deleteExpenses from "./delete/deleteExpenses";
 
 const app = express();
 app.use(express.json());
@@ -11,5 +12,6 @@ app.use(getExpense);
 app.use(getExpenseById);
 app.use(postExpenses);
 app.use(putExpenses);
+app.use(deleteExpenses);
 
 app.listen(3000, () => "server is running in port 3000");
