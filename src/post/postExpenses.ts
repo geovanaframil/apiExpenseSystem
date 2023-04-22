9facf8913dbeb1dde50ec38bf57bbbda3026572c
Geovana Framil <geovanaframil@hotmail.com>
2023-04-22 09:23:12 -0300

    Adiciona variáveis que irão verificar se a categoria e o usuário existem nos arrays correspondentes

--- src/post/postExpenses.ts
+++ src/post/postExpenses.ts
@@ -1,16 +1,22 @@
 import { Router, Request, Response } from "express";
 import createIdByExpense from "../utils/createIdByExpense";
-import { IExpense, IExpenseBodyPost, IExpenseByUser } from "../interfaces/expenseInterface";
+import {
+  IExpense,
+  IExpenseBodyPost,
+  IExpenseByUser,
+} from "../interfaces/expenseInterface";
 import postBodySchema from "../utils/verifyBodyExpense";
 import saveData from "../utils/saveDataJson";
+import ICategory from "../interfaces/categoryInterface";
+import User from "../interfaces/userInterface";
 
 const router = Router();
 
-const data = require("../../database/expenses.json");
-const expenses: IExpenseBodyPost[] = data;
-const users = require("../../database/users.json");
+const expensesData = require("../../database/expenses.json");
+const usersData = require("../../database/users.json");
+const categoriesData = require("../../database/categories.json");
 
-console.log(users);
+console.log(usersData);
 
 function handleBodyExpenseRegister(
   returnAPI: any,
@@ -28,17 +34,38 @@ function handleBodyExpenseRegister(
 }
 
 router.post("/expenses", (req: Request, res: Response) => {
+  const expenses: IExpense[] = expensesData;
+  const categories: ICategory[] = categoriesData;
+  const users: User[] = usersData;
   const body: IExpenseBodyPost = req.body;
   const { error } = postBodySchema.validate(body);
   if (error) {
-    res.status(400).json({ message: error.details[0].message });
-  } else {
-    const IdExpense = createIdByExpense(body.name, body.userID);
-    const currentExpense = handleBodyExpenseRegister(body, IdExpense);
-    expenses.push(currentExpense);
-    res.status(200).json({ message: "usuário cadastrado com sucesso" });
-    saveData(expenses, "expenses");
+    res.status(400).json({
+      message: "Corpo inválido",
+      bodyExpected: {
+        name: "string",
+        categoryID: "string",
+        userID: "string",
+        amount: "number",
+        status: "PAGO || PENDENTE",
+      },
+    });
   }
+  // else {
+  //   const IdExpense = createIdByExpense(body.name, body.userID);
+  //   const currentExpense = handleBodyExpenseRegister(body, IdExpense);
+  //   expenses.push(currentExpense);
+  //   res.status(200).json({ message: "usuário cadastrado com sucesso" });
+  //   saveData(expenses, "expenses");
+  // }
+
+  const { categoryID, userID } = body;
+  // Verifica se a cateforia e o usuário foram encontrados nos database "categories" e "users", respectivamente
+  const foundCategory = categories.find(
+    (category) => category.id === categoryID
+  );
+  const foundUser = users.find((user) => user.id === userID);
+  const searchIndexUser = users.findIndex((user) => user.id === userID);
 });
 
 export default router;
