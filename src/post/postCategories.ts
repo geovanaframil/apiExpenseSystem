a969d9ccf8eb82d83ac3bcf3bee91abcd2139bcf
Rheingantz <pedrorpiragibe@gmail.com>
2023-04-22 18:51:11 -0300

    implementando verificação por JOI no POST categories

--- src/post/postCategories.ts
+++ src/post/postCategories.ts
@@ -1,13 +1,17 @@
 import { Router, Request, Response } from "express";
 import saveData from "../utils/saveDataJson";
-import readData from "../utils/readDataJson";
 import ICategory from "../interfaces/categoryInterface";
 import createIdByCategory from "../utils/createIdByCategory";
-import verifyBodyCategory from "../utils/verifyBodyCategory";
 
 const router = Router();
+const Joi = require('joi')
 const data = require("../../database/categories.json");
 const categories: ICategory[] = data;
+
+const schema = Joi.object({
+  id: Joi.string(),
+  name: Joi.string().required()
+});
 
 function handleBodyCategory(returnAPI: any, idCategory: string): ICategory {
   const newCategory = {
@@ -19,15 +23,15 @@ function handleBodyCategory(returnAPI: any, idCategory: string): ICategory {
 
 router.post("/categories", (req: Request, res: Response) => {
   const body = req.body;
-  const validBody = verifyBodyCategory(body);
-  if (validBody.isValid) {
+  const { error, value } =  schema.validate(req.body)
+  if (!error) {
     const newIdCategory = createIdByCategory(body.name);
     const category = handleBodyCategory(body, newIdCategory);
     categories.push(category);
     res.status(200).json("categoria cadastrada com sucesso");
     saveData(categories, "categories");
   } else {
-    res.status(400).json({message: validBody.message});
+    res.status(404).send(error.details[0].message);
   }
 });
 
