40ef55a698b6c1744c0f274c32ab23cfeb385325
Kayk magalhaes <kaykmagalhaesdiv@gmail.com>
2023-04-24 15:13:44 -0300

    Merge branch 'main'

--- src/interfaces/expenseInterface.ts
+++ src/interfaces/expenseInterface.ts
@@ -1,41 +1,42 @@
 interface IExpense {
   id:string,
-  name: string;
-  categoryID: string;
-  userID: string;
-  amount: number;
-  status: string;
-  _user: IUser;
-  _category: ICategory;
+  name: string
+  categoryID: string
+  userID: string
+  amount: number
+  status: string
+  _user: IUser
+  _category: ICategory
 }
 
 export interface IExpenseUser {
-  id: string;
-  name: string;
-  categoryID: string;
-  userID: string;
-  amount: number;
-  status: string;
-  _category: ICategory;
+  id: string
+  name: string
+  categoryID: string
+  userID: string
+  amount: number
+  status: string
+  _category: ICategory
 }
 
 export interface IExpensePost {
-  name: string;
-  categoryID: string;
-  userID: string;
-  amount: number;
+  name: string
+  categoryID: string
+  userID: string
+  amount: number
+  status: string
 }
 
 interface IUser {
-  id: string;
-  name: string;
-  lastName: string;
-  email: string;
+  id: string
+  name: string
+  lastName: string
+  email: string
 }
 
 interface ICategory {
-  id: string;
-  name: string;
+  id: string
+  name: string
 }
 
-export default IExpense;
+export default IExpense
